7441e9e63715622c7114a1776dc6d4ccbdf2443d
hyunsoo <jing07161@gmail.com>
2023-12-31 16:38:40 +0900

    feature: 로딩창 구현

---
+++ app/loading.tsx
@@ -0,0 +1,8 @@
+const Loading = () => {
+    return(
+    <div className="fixed w-full h-full inset-0 bg-black bg-opacity-50 z-50 flex justify-center items-center">
+        <div className="text-white text-2xl">Loading...</div>
+    </div>
+    )
+}
+export default Loading;
