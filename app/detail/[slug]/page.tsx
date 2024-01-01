540210a1cf690978b66d59711aecbbe57185ee89
hyunsoo <jing07161@gmail.com>
2024-01-01 23:54:55 +0900

    feature: 공휴일 제외, 공휴일 포함 목록 추가 구현

--- app/detail/[slug]/page.tsx
+++ app/detail/[slug]/page.tsx
@@ -24,17 +24,20 @@ const Page = ({ params }: { params: { slug: string }}) => {
         // setWorkRecord(...workInfo);
         setWorkInfo(workInfo);
     }, [params.slug, getWorkerList])
-
+    console.log('workInfo', workInfo.userName, workInfo.workRecord);
     return (
         <main className={'md:container xl:mx-auto mx-2 h-full flex flex-col justify-center items-center py-10'}>
-            <div className={'h-12 w-full xl:text-xl text-sm flex gap-5'}>
+            <div className={'h-12 w-full xl:text-xl text-sm flex gap-5 justify-center'}>
                 <div>{workInfo.userName}</div>
                 <div>차감 근무: {workInfo.totalUnderWorkTime}</div>
                 <div>초과 근무: {workInfo.totalOverTime}</div>
             </div>
-            <div className={'h-12 w-full xl:text-xl text-sm flex gap-5'}>
+            <div className={'h-12 w-full xl:text-xl text-sm flex gap-5 justify-center'}>
                 <div>실 초과 근무 합계: {workInfo.totalTime}</div>
                 <div>{workInfo.vacation}</div>
+            </div>
+            <div className={'h-12 w-full xl:text-xl text-sm flex gap-5'}>
+                <div>근무 기록</div>
             </div>
             <table className="w-full table-auto border border-indigo-600 xl:text-xl text-sm">
                 <thead>
@@ -42,6 +45,7 @@ const Page = ({ params }: { params: { slug: string }}) => {
                     <th className={'hidden xl:table-cell'}>부서</th>
                     <th>성함</th>
                     <th className={'hidden xl:table-cell'}>근무 일자</th>
+                    <th className={'hidden xl:table-cell'}>상태</th>
                     <th>출근 시간</th>
                     <th>퇴근 시간</th>
                     <th>초과 근무</th>
@@ -55,6 +59,7 @@ const Page = ({ params }: { params: { slug: string }}) => {
                             <td className="text-center align-middle hidden xl:table-cell">{record.department ? record.department : '--'}</td>
                             <td className="text-center align-middle">{record.name ? record.name : '--'}</td>
                             <td className="text-center align-middle hidden xl:table-cell">{record.date ? record.date : '--'}</td>
+                            <td className="text-center align-middle hidden xl:table-cell">{record.state ? record.state : '--'}</td>
                             <td className="text-center align-middle">{record.workingTime ? record.workingTime : '--'}</td>
                             <td className="text-center align-middle">{record.leaveTime ? record.leaveTime : '--'}</td>
                             <td className="text-center align-middle">{record.overTime ? record.overTime : '--'}</td>
@@ -67,7 +72,7 @@ const Page = ({ params }: { params: { slug: string }}) => {
             </table>
 
             <div className={'h-12 w-full xl:text-xl text-sm flex items-center gap-5 mt-5'}>
-                <div>휴가 대상 목록</div>
+                <div>휴가 대상 목록 (공휴일 제외)</div>
             </div>
             <table className="w-full table-auto border border-indigo-600 xl:text-xl text-sm">
                 <thead>
@@ -75,6 +80,7 @@ const Page = ({ params }: { params: { slug: string }}) => {
                     <th className={'hidden xl:table-cell'}>부서</th>
                     <th>성함</th>
                     <th className={'hidden xl:table-cell'}>근무 일자</th>
+                    <th className={'hidden xl:table-cell'}>상태</th>
                     <th>출근 시간</th>
                     <th>퇴근 시간</th>
                     <th>초과 근무</th>
@@ -82,12 +88,47 @@ const Page = ({ params }: { params: { slug: string }}) => {
                 </tr>
                 </thead>
                 <tbody>
-                {workInfo.workRecord.filter((record:InfoEn) => convertToSeconds(getPositiveTimesAfterSubtracting2Hours(record.overTime)) > 0).map((record:InfoEn, idx) => {
+                {workInfo.workRecord.filter((record:InfoEn) => convertToSeconds(getPositiveTimesAfterSubtracting2Hours(record.overTime)) > 0).filter((record) => record.state !== '공휴일').map((record:InfoEn, idx) => {
                     return (
                         <tr key={idx} className={'border border-indigo-500 h-8 hover:bg-indigo-500 cursor-pointer'}>
                             <td className="text-center align-middle hidden xl:table-cell">{record.department ? record.department : '--'}</td>
                             <td className="text-center align-middle">{record.name ? record.name : '--'}</td>
                             <td className="text-center align-middle hidden xl:table-cell">{record.date ? record.date : '--'}</td>
+                            <td className="text-center align-middle hidden xl:table-cell">{record.state ? record.state : '--'}</td>
+                            <td className="text-center align-middle">{record.workingTime ? record.workingTime : '--'}</td>
+                            <td className="text-center align-middle">{record.leaveTime ? record.leaveTime : '--'}</td>
+                            <td className="text-center align-middle">{record.overTime ? record.overTime : '--'}</td>
+                            <td className="text-center align-middle">{record.overTime ? getPositiveTimesAfterSubtracting2Hours(record.overTime) : '--'}</td>
+                        </tr>
+                    )
+                })}
+
+                </tbody>
+            </table>
+            <div className={'h-12 w-full xl:text-xl text-sm flex items-center gap-5 mt-5'}>
+                <div>휴가 대상 목록 (공휴일)</div>
+            </div>
+            <table className="w-full table-auto border border-indigo-600 xl:text-xl text-sm">
+                <thead>
+                <tr className={'bg-indigo-600 h-12'}>
+                    <th className={'hidden xl:table-cell'}>부서</th>
+                    <th>성함</th>
+                    <th className={'hidden xl:table-cell'}>근무 일자</th>
+                    <th className={'hidden xl:table-cell'}>상태</th>
+                    <th>출근 시간</th>
+                    <th>퇴근 시간</th>
+                    <th>초과 근무</th>
+                    <th>휴가 대상</th>
+                </tr>
+                </thead>
+                <tbody>
+                {workInfo.workRecord.filter((record:InfoEn) => convertToSeconds(getPositiveTimesAfterSubtracting2Hours(record.overTime)) > 0).filter((record) => record.state === '공휴일').map((record:InfoEn, idx) => {
+                    return (
+                        <tr key={idx} className={'border border-indigo-500 h-8 hover:bg-indigo-500 cursor-pointer'}>
+                            <td className="text-center align-middle hidden xl:table-cell">{record.department ? record.department : '--'}</td>
+                            <td className="text-center align-middle">{record.name ? record.name : '--'}</td>
+                            <td className="text-center align-middle hidden xl:table-cell">{record.date ? record.date : '--'}</td>
+                            <td className="text-center align-middle hidden xl:table-cell">{record.state ? record.state : '--'}</td>
                             <td className="text-center align-middle">{record.workingTime ? record.workingTime : '--'}</td>
                             <td className="text-center align-middle">{record.leaveTime ? record.leaveTime : '--'}</td>
                             <td className="text-center align-middle">{record.overTime ? record.overTime : '--'}</td>
